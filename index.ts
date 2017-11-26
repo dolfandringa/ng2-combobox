513d19178f2f75b752f313dc4b8a61bba586dd96
Dolf Andringa <dolfandringa@gmail.com>
2017-11-26 21:29:38 +0800

    Move index.ts to src

--- index.ts
+++
@@ -1,3 +0,0 @@
-export * from './src/combo-box.component';
-export * from './src/combo-box-entitty.interface';
-export * from './src/combo-box.module';
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from './combo-box.component';
+export * from './combo-box-entitty.interface';
+export * from './combo-box.module';
